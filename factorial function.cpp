d3c48c0625e7c8da172c82e52e60f053bd5fdd40
Sasindu Sachintha <sasindusachintha1234@gmail.com>
2026-01-11 16:34:06 +0530

    Added comments to factorial.cpp

--- factorial function.cpp
+++ factorial function.cpp
@@ -1,16 +1,21 @@
-# include <iostream>
-
+#include <iostream>
 using namespace std;
 
-int factorial(int n){
-   if(n == 1){
-   	return 1;
-   }
-   else{
-   		return (n* factorial(n-1));
-   }
+// Function to calculate factorial of a number recursively
+int factorial(int n) {
+    // Base case: factorial of 1 is 1
+    if (n == 1) {
+        return 1;
+    } 
+    // Recursive case: n * factorial of (n-1)
+    else {
+        return n * factorial(n - 1);
+    }
 }
- 
-int main(){
-	cout << "n! = " << factorial(5);
+
+int main() {
+    int num = 5; // Number to calculate factorial
+    cout << num << "! = " << factorial(num) << endl; // Print factorial
+    return 0;
 }
+
